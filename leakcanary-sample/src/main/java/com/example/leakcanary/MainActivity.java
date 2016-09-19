45d52d48ffb0979833737f6305d6298a7fcbbb8c
lingxiang.wang <lingxiang.wang@alibaba-inc.com>
2016-09-19 14:27:29 +0800

    can run

--- leakcanary-sample/src/main/java/com/example/leakcanary/MainActivity.java
+++ leakcanary-sample/src/main/java/com/example/leakcanary/MainActivity.java
@@ -19,34 +19,59 @@ import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends Activity {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main_activity);
+    private String TAG = "MainActivity";
 
-    View button = findViewById(R.id.async_task);
-    button.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        startAsyncTask();
-      }
-    });
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
 
-  void startAsyncTask() {
-    // This async task is an anonymous class and therefore has a hidden reference to the outer
-    // class MainActivity. If the activity gets destroyed before the task finishes (e.g. rotation),
-    // the activity instance will leak.
-    new AsyncTask<Void, Void, Void>() {
-      @Override protected Void doInBackground(Void... params) {
-        // Do some slow work in background
-        SystemClock.sleep(20000);
-        return null;
-      }
-    }.execute();
-  }
+        View button = findViewById(R.id.async_task);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAsyncTask();
+            }
+        });
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    void startAsyncTask() {
+        // This async task is an anonymous class and therefore has a hidden reference to the outer
+        // class MainActivity. If the activity gets destroyed before the task finishes (e.g. rotation),
+        // the activity instance will leak.
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                // Do some slow work in background
+                SystemClock.sleep(20000);
+                return null;
+            }
+        }.execute();
+    }
 }
 
 
